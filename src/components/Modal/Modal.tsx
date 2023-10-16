980a999e5c961709a9a73056d9463985ff65ea0f
Nikita <nickpelmog2003@gmail.com>
2023-10-17 00:37:10 +0300

    INT-2/FEAT: add onclicks

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,12 +1,12 @@
 import styles from './Modal.module.scss'
 import React from "react";
-const Modal = () => {
+const Modal = ({ closeModal }) => {
     return <div className={styles.mainContainer}>
         <img src="src/assets/logo.png" alt="Logo" height="200"/>
         <p className={styles.walletTitleGap}>Кошелек добра</p>
         <p className={styles.walletTitle}>Сделай первый шаг на пути к добру</p>
         <p className={styles.subTitle}>Мы расскажем как можно начать помогать даже если денег нет </p>
-        <button className={styles.buttonRegister}>Регистрация</button>
+        <button className={styles.buttonRegister} onClick={closeModal}>Регистрация</button>
         <a className={styles.linkLogin}>Войти</a>
     </div>
 }
